d0d9dac17719a5309c2448a9e965c232b0a006dc
Barshal <hectormovilmur@gmail.com>
2025-12-16 11:02:46 +0100

    Pantalla de login basica

--- app/src/main/java/com/example/gestorsiniestros/MainActivity.kt
+++ app/src/main/java/com/example/gestorsiniestros/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.gestorsiniestros
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gestorsiniestros.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    // Creamo el binding
+    private lateinit var binding: ActivityMainBinding // No inicializar aquí. Lo haremos más tarde.
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Despues de esta linea puedo hacer lo que quiera con los elementos gráficos del layout
+
+    }
+    private fun acciones(){
+
+        binding.btnAcceder.setOnClickListener(this)
+        binding.cbRecordarUsuario.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            binding.btnAcceder.id -> {}
+            binding.cbRecordarUsuario.id -> {}
         }
     }
 }
